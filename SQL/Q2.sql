646fb227e476a755d82520a8081f3251beb0b8dc
Aniruddha Sarkar <145282516+Ani811625@users.noreply.github.com>
2025-06-08 10:22:20 +0530

    Delete SQL directory

--- SQL/Q2.sql
+++
@@ -1,8 +0,0 @@
--- Q2: List of all customers where company names end with 'N'
-SELECT 
-    s.BusinessEntityID AS StoreID,
-    s.Name AS StoreName
-FROM 
-    Sales.Store s
-WHERE 
-    s.Name LIKE '%N';
--- SQL/Q7.sql
+++
@@ -1,9 +0,0 @@
--- Q7: List of customers who placed at least one order
-SELECT DISTINCT
-    c.CustomerID,
-    ISNULL(p.FirstName + ' ' + p.LastName, s.Name) AS CustomerName
-FROM 
-    Sales.Customer c
-LEFT JOIN Person.Person p ON c.PersonID = p.BusinessEntityID
-LEFT JOIN Sales.Store s ON c.StoreID = s.BusinessEntityID
-JOIN Sales.SalesOrderHeader soh ON c.CustomerID = soh.CustomerID;
